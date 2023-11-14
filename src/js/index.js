3f6f5eed675e25954fe9a00cd4d64449a8147dac
Ninuskaninus <89078328+Ninuskaninus@users.noreply.github.com>
2023-11-14 17:28:12 +0100

    added get request to fetch all listings

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,4 @@
-import {htmlDesign} from './design/design.js';
+import { htmlDesign } from "./design/design.js";
+import { getListings } from "./API/GET/getListings.js";
 htmlDesign();
+getListings();
