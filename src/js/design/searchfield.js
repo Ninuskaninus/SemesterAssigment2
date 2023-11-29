813681334fc4c83538fad0e008251731fdb0ac45
Ninuskaninus <89078328+Ninuskaninus@users.noreply.github.com>
2023-11-29 22:37:24 +0100

    Made the search function work

--- src/js/design/searchfield.js
+++ src/js/design/searchfield.js
@@ -1,12 +1,13 @@
-import { searchBar } from "../sorting/search.js";
+import { getListings } from "../API/GET/getListings.js";
 
-export function searchField() {
+export async function searchField() {
   const mainContainer = document.getElementById("maincontainer-index");
   const searchField = document.createElement("div");
   searchField.classList.add("searchBar");
   mainContainer.appendChild(searchField);
 
   const searchForm = document.createElement("form");
+  searchForm.id = "search-form";
   searchField.appendChild(searchForm);
 
   const searchInput = document.createElement("input");
@@ -24,6 +25,54 @@ export function searchField() {
   searchIcon.src = "/src/assets/search.png";
   searchIcon.alt = "search";
   searchButton.appendChild(searchIcon);
-}
 
-searchBar();
+  const searchResults = document.createElement("div");
+  searchResults.id = "search-results";
+  searchResults.classList.add("search-results");
+  searchField.appendChild(searchResults);
+
+  const resultTitle = document.createElement("h2");
+  resultTitle.innerHTML = "Search Results";
+  searchResults.appendChild(resultTitle);
+
+  searchButton.addEventListener("click", async () => {
+    const searchValue = searchInput.value;
+    const allListings = await getListings();
+    searchResults.innerHTML = "";
+    searchResults.style.display = "block";
+    
+    if (Array.isArray(allListings)) {
+      const searchResult = allListings.filter((listing) =>
+        listing.title.toLowerCase().includes(searchValue.toLowerCase())
+      );
+      console.log(searchResult);
+
+      searchResults.innerHTML = "";
+
+      // Iterate over search results and create elements
+      searchResult.forEach((listing) => {
+        const searchResultItem = document.createElement("div");
+        searchResultItem.classList.add("search-result-item");
+        searchResults.appendChild(searchResultItem);
+
+        const searchResultImage = document.createElement("div");
+        searchResultImage.classList.add("search-result-image");
+        searchResultImage.style.backgroundImage = `url(${listing.media})`;
+        searchResultItem.appendChild(searchResultImage);
+
+        const searchResultTitle = document.createElement("p");
+        searchResultTitle.classList.add("search-result-title");
+        searchResultTitle.innerHTML = listing.title;
+        searchResultItem.appendChild(searchResultTitle);
+
+        const searchResultBtn = document.createElement("button");
+        searchResultBtn.classList.add("mainBtn", "btn");
+        searchResultBtn.innerHTML = "View";
+        searchResultItem.appendChild(searchResultBtn);
+
+      });
+    } else {
+      console.error("Invalid data received from getListings");
+    }
+  });
+}
--- src/js/sorting/search.js
+++ src/js/sorting/search.js
@@ -1,9 +1,19 @@
 import { getListings } from "../API/GET/getListings.js";
 const allListings = await getListings();
 
-export function searchBar() {
-  const listingsTags = allListings.map((listing) => listing.tags);
-  const tags = listingsTags.flat();
-  const uniqueTags = [...new Set(tags)];
-  console.log(tags);
+let searchResult = [];
+
+export function searchBar(searchForm) {
+  
+    const listingsTags = allListings.map((listing) => listing.tags);
+    const tags = listingsTags.flat();
+    const uniqueTags = [...new Set(tags)];
+
+    const searchInput = document.getElementById("search");
+    const searchButton = document.getElementById("search-button");
+
+
+
 }
+
+
